2e18d6bd7ff49574099565d18a7f6365a0522aaa
Ben Scheirman <ben@scheirman.com>
2024-04-24 19:16:08 -0500

    Episode 475

---
+++ Sources/App/Migrations/CreateArtists.swift
@@ -0,0 +1,23 @@
+import Fluent
+
+struct CreateArtists: AsyncMigration {
+    func prepare(on database: any Database) async throws {
+        try await database.schema("artists")
+            .id()
+            .timestamps()
+            .field("name", .string, .required)
+            .create()
+
+        try await database.schema("songs")
+            .field("artist_id", .uuid, .references("artists", "id"))
+            .update()
+    }
+
+    func revert(on database: any Database) async throws {
+        try await database.schema("songs")
+            .deleteField("artist_id")
+            .update()
+
+        try await database.schema("artists").delete()
+    }
+}
